b20942f3c4d5f8f8dd051edc868f579f6bfab058
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2024-12-03 12:29:46 +0100

    Add basic test coverage for nomination resetting

--- osu.Server.BeatmapSubmission.Tests/BeatmapSubmissionControllerTest.cs
+++ osu.Server.BeatmapSubmission.Tests/BeatmapSubmissionControllerTest.cs
@@ -32,6 +32,7 @@ namespace osu.Server.BeatmapSubmission.Tests
         protected new HttpClient Client { get; }
 
         private readonly LocalBeatmapStorage beatmapStorage;
+        private readonly Mock<ILegacyIO> mockLegacyIO = new Mock<ILegacyIO>();
 
         public BeatmapSubmissionControllerTest(IntegrationTestWebApplicationFactory<Program> webAppFactory)
             : base(webAppFactory)
@@ -44,7 +45,7 @@ namespace osu.Server.BeatmapSubmission.Tests
                 {
                     services.AddTransient<IBeatmapStorage>(_ => beatmapStorage);
                     services.AddTransient<BeatmapPackagePatcher>();
-                    services.AddTransient<ILegacyIO>(_ => new Mock<ILegacyIO>().Object);
+                    services.AddTransient<ILegacyIO>(_ => mockLegacyIO.Object);
                 });
             }).CreateClient();
         }
@@ -907,6 +908,7 @@ namespace osu.Server.BeatmapSubmission.Tests
 
             var renamedBeatmap = await db.QuerySingleAsync<osu_beatmap>(@"SELECT * FROM `osu_beatmaps` WHERE `beatmap_id` = 557810");
             Assert.Equal("Platter 2", renamedBeatmap.version);
+            mockLegacyIO.Verify(io => io.DisqualifyBeatmapSetAsync(241526, It.IsAny<string>()), Times.Never);
         }
 
         [Fact]
@@ -944,6 +946,39 @@ namespace osu.Server.BeatmapSubmission.Tests
             Assert.True(response.IsSuccessStatusCode);
 
             WaitForDatabaseState(@"SELECT COUNT(1) FROM `osu_beatmapsets` WHERE `beatmapset_id` = 241526 AND `deleted_at` IS NULL AND `storyboard` = 1", 1, CancellationToken);
+        }
+
+        [Fact]
+        public async Task TestPatchPackage_NominationsReset()
+        {
+            using var db = await DatabaseAccess.GetConnectionAsync();
+            await db.ExecuteAsync("INSERT INTO `phpbb_users` (`user_id`, `username`, `country_acronym`, `user_permissions`, `user_sig`, `user_occ`, `user_interests`) VALUES (1000, 'test', 'JP', '', '', '', '')");
+
+            await db.ExecuteAsync(@"INSERT INTO `osu_beatmapsets` (`beatmapset_id`, `user_id`, `creator`, `approved`, `thread_id`, `active`, `submit_date`) VALUES (241526, 1000, 'test user', -1, 0, -1, CURRENT_TIMESTAMP)");
+
+            foreach (uint beatmapId in new uint[] { 557815, 557814, 557821, 557816, 557817, 557818, 557812, 557810, 557811, 557820, 557813, 557819 })
+                await db.ExecuteAsync(@"INSERT INTO `osu_beatmaps` (`beatmap_id`, `user_id`, `beatmapset_id`, `approved`) VALUES (@beatmapId, 1000, 241526, -1)", new { beatmapId = beatmapId });
+
+            await db.ExecuteAsync("INSERT INTO `beatmapset_events` (`beatmapset_id`, `user_id`, `type`) VALUES (241526, 1001, 'nominate')");
+
+            using (var dstStream = File.OpenWrite(Path.Combine(beatmapStorage.BaseDirectory, "241526")))
+            using (var srcStream = TestResources.GetResource(osz_filename)!)
+                await srcStream.CopyToAsync(dstStream);
+
+            var request = new HttpRequestMessage(HttpMethod.Patch, "/beatmapsets/241526");
+
+            using var content = new MultipartFormDataContent($"{Guid.NewGuid()}----");
+            using var osuFileStream = TestResources.GetResource(osu_filename)!;
+            content.Add(new StreamContent(osuFileStream), "filesChanged", osu_filename);
+            content.Add(new StringContent("Soleily - Renatus (Deif) [Platter].osu"), "filesDeleted");
+            request.Content = content;
+            request.Headers.Add(HeaderBasedAuthenticationHandler.USER_ID_HEADER, "1000");
+
+            var response = await Client.SendAsync(request);
+            Assert.True(response.IsSuccessStatusCode);
+
+            WaitForDatabaseState(@"SELECT COUNT(1) FROM `osu_beatmaps` WHERE `beatmapset_id` = 241526 AND `deleted_at` IS NULL", 12, CancellationToken);
+            mockLegacyIO.Verify(io => io.DisqualifyBeatmapSetAsync(241526, It.IsAny<string>()), Times.AtLeastOnce);
         }
 
         [Fact]
